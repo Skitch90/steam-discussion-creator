280632f0791b4857202185a6ab57cc85ef0adb5d
Alessandro Schio <ale.schio90@gmail.com>
2024-03-02 09:26:19 +0100

    Change css selector for username input field and add waiting for login page load

--- src/main/java/it/alesc/steam/pages/LoginPage.kt
+++ src/main/java/it/alesc/steam/pages/LoginPage.kt
@@ -1,12 +1,15 @@
 package it.alesc.steam.pages
 
+import org.openqa.selenium.By
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.WebElement
 import org.openqa.selenium.support.FindBy
+import org.openqa.selenium.support.ui.WebDriverWait
+import java.time.Duration
 
 class LoginPage(driver: WebDriver): BasePage(driver) {
 
-    @FindBy(css = "form[class^='newlogindialog'] > div > input[type='text']")
+    @FindBy(css = "input[type='text']")
     private val usernameTextField: WebElement? = null
 
     @FindBy(css = "input[type='password']")
@@ -17,6 +20,8 @@ class LoginPage(driver: WebDriver): BasePage(driver) {
 
     fun navigate() {
         navigateTo("https://store.steampowered.com/login/")
+        WebDriverWait(driver, Duration.ofSeconds(10))
+            .until { it.findElement(By.cssSelector("input[type='password']")) }
     }
 
     fun insertCredentials(username: String, password: String) {
